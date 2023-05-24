c0ea39ca70fa5a19913a4dbe808551c2bcab3fd4
LuK1337 <priv.luk@gmail.com>
2023-05-24 23:36:18 +0700

    sm8250-common: Unmount /firmware after fastbootd starts
    Fixes `fastboot flash modem modem.img`.

--- common.mk
+++ common.mk
@@ -344,6 +344,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
